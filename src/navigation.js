2d1a3f3413e7b874a6c57b82f21702e2e2163543
Elizabeth Zhu <elizabethzhuca@gmail.com>
2023-09-11 15:31:34 -0700

    fixes

--- src/navigation.js
+++ src/navigation.js
@@ -24,94 +24,23 @@ export const headerData = {
     //   ],
     // },
     {
-      text: 'Pages',
-      links: [
-        {
-          text: 'Features (Anchor Link)',
-          href: getPermalink('/#features'),
-        },
-        {
-          text: 'Services',
-          href: getPermalink('/services'),
-        },
-        {
-          text: 'Pricing',
-          href: getPermalink('/pricing'),
-        },
-        {
-          text: 'About us',
-          href: getPermalink('/about'),
-        },
-        {
-          text: 'Contact',
-          href: getPermalink('/contact'),
-        },
-        {
-          text: 'Terms',
-          href: getPermalink('/terms'),
-        },
-        {
-          text: 'Privacy policy',
-          href: getPermalink('/privacy'),
-        },
-      ],
+      text: 'About',
+      href: '#',
     },
     {
-      text: 'Blog',
-      links: [
-        {
-          text: 'Blog List',
-          href: getBlogPermalink(),
-        },
-        {
-          text: 'Article',
-          href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
-        },
-        {
-          text: 'Article (with MDX)',
-          href: getPermalink('markdown-elements-demo-post', 'post'),
-        },
-        {
-          text: 'Category Page',
-          href: getPermalink('tutorials', 'category'),
-        },
-        {
-          text: 'Tag Page',
-          href: getPermalink('astro', 'tag'),
-        },
-      ],
-    },
-    {
-      text: 'Podcast',
-      // links: [
-      //   {
-      //     text: 'Lead Generation',
-      //     href: getPermalink('/landing/lead-generation'),
-      //   },
-      //   {
-      //     text: 'Long-form Sales',
-      //     href: getPermalink('/landing/sales'),
-      //   },
-      //   {
-      //     text: 'Click-Through',
-      //     href: getPermalink('/landing/click-through'),
-      //   },
-      //   {
-      //     text: 'Product Details (or Services)',
-      //     href: getPermalink('/landing/product'),
-      //   },
-      //   {
-      //     text: 'Coming Soon or Pre-Launch',
-      //     href: getPermalink('/landing/pre-launch'),
-      //   },
-      //   {
-      //     text: 'Subscription',
-      //     href: getPermalink('/landing/subscription'),
-      //   },
-      // ],
+      text: 'Submit',
+      href: '#',
     },
     {
       text: 'Journal',
+      href: '#',
+    },
+    {
+      text: 'Podcast',
+      href: '#',
+    },
+    {
+      text: 'Community',
       href: '#',
     },
   ],
